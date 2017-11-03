a04d7a910a8afa46a7fb700a9613c68ca1876517
papistan <jpapisan@gmail.com>
2017-11-02 20:01:19 -0700

    links account

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="hello">
-    
+    <h1>{{ msg }}</h1>
+    <h3>{{ showAccount }}</h3>
   </div>
 </template>
 
 <script>
+import web3 from 'web3'
+
+
+let provider = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+provider.eth.defaultAccount = provider.eth.accounts[0]
+
+let MyContract = provider.eth.contract([{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"x","type":"bytes32"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]);
+
+let contract = MyContract.at('0x43ea5b0ee2a81dcef4c1f794afa697f2b27ce680')
+
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'hhhhhhhhhhhh'
+      msg: 'Current Account:: '
+    }
+  },
+  computed: {
+    showAccount: function() {
+      return this.msg + provider.eth.defaultAccount
     }
   }
 }
